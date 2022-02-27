59339427a0e835bed19dfaa173b7f743129832d3
Thiago Porto <thiago.porto@d3.do>
2022-02-27 15:23:15 -0300

    finish app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
+import { RootState } from 'store';
+import Search from 'components/Search';
+import Alert from 'components/Alert';
+import Weather from 'components/Weather';
+import { setAlert } from 'store/actions/alertActions';
+import { setError } from 'store/actions/weatherActions';
+
 const App: FC = () => {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+  const weatherData = useSelector((state: RootState) => state.weather.data);
+  const loading = useSelector((state: RootState) => state.weather.loading);
+  const error = useSelector((state: RootState) => state.weather.error);
+  const alertMsg = useSelector((state: RootState) => state.alert.message);
+
+  const onCloseAlert = (): void => {
+    dispatch(setAlert(''));
+  };
+
+  const onCloseError = (): void => {
+    dispatch(setError());
+  };
+
+  return (
+    <div className="has-text-centered">
+      <Search title="Enter city name and press search button" />
+      {loading ? (
+        <h2 className="is-size-3 py-2">Loading...</h2>
+      ) : (
+        weatherData && <Weather data={weatherData} />
+      )}
+
+      {alertMsg && <Alert message={alertMsg} onClose={onCloseAlert} />}
+
+      {error && <Alert message={error} onClose={onCloseError} />}
+    </div>
+  );
 };
 
 export default App;
