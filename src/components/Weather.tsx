a1c93833f10cbe1efae60dbaf26cf5076f653526
Thiago Porto <thiago.porto@d3.do>
2022-02-27 15:23:44 -0300

    fix Weather's styling

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -12,45 +12,47 @@ const Weather: React.FC<WeatherProps> = ({ data }) => {
   return (
     <section className="section">
       <div className="container">
-        <h1 className="title has-text-centered">
+        <h1 className="title has-text-centered" style={{ marginBottom: 50 }}>
           {data.name} - {data.sys.country}
         </h1>
         <div className="level" style={{ alignItems: 'flex-start' }}>
-          <div>
-            <p className="heading">{data.weather[0].description}</p>
-            <p className="title">
-              <img
-                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-                alt={data.weather[0].description}
-              />
-            </p>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">{data.weather[0].description}</p>
+              <p className="title">
+                <img
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                  alt=""
+                />
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="level-item has-text-centered">
-          <div>
-            <p className="heading">temp</p>
-            <div className="title">
-              <p className="mb-2">{data.main.temp}K</p>
-              <p className="mb-2">
-                {fahrenheit}
-                <sup>&#8457;</sup>
-              </p>
-              <p className="mb-2">
-                {celsius}
-                <sup>&#8451;</sup>
-              </p>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">temp</p>
+              <div className="title">
+                <p className="mb-2">{data.main.temp}K</p>
+                <p className="mb-2">
+                  {fahrenheit}
+                  <sup>&#8457;</sup>
+                </p>
+                <p>
+                  {celsius}
+                  <sup>&#8451;</sup>
+                </p>
+              </div>
             </div>
           </div>
           <div className="level-item has-text-centered">
             <div>
               <p className="heading">humidity</p>
-              <p className="title">{data.main.humidity}</p>
+              <p className="title">{data.main.humidity}%</p>
             </div>
           </div>
           <div className="level-item has-text-centered">
             <div>
               <p className="heading">pressure</p>
-              <p className="title">{data.main.pressure}</p>
+              <p className="title">{data.main.pressure} hPa</p>
             </div>
           </div>
           <div className="level-item has-text-centered">
