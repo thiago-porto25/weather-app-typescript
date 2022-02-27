f75d9f8df7995bbcaae7803e819f1870c1d12d2a
Thiago Porto <thiago.porto@d3.do>
2022-02-27 14:35:10 -0300

    make store avaliable

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
+import { Provider } from 'react-redux';
+import store from 'store';
+import 'bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
